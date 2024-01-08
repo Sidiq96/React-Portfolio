231b03814e80c76f1fe4fbf1473d6fe9af4dc16c
Sidiq Hersi <hersisidiq@gmail.com>
2024-01-08 19:12:28 +0000

    fixes a bug in the mobile view where the dropdown menu appears when the page refreshes, so now it only appears when the dropdown menu is clicked

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
 import { FaBars } from "react-icons/fa";
 import Logo from "../assets/images/logo.png";
+
 const NavBar = () => {
   const [nav, setNav] = useState(false);
-  const handleClick = () => setNav(!nav);
+
+  const handleClick = () => {
+    setNav(!nav);
+  };
+
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
@@ -13,8 +18,9 @@ const NavBar = () => {
           style={{ width: "110px", height: "110px" }}
         />
       </div>
-      {/* menu */}
-      <ul className="hidden md:flex ">
+
+      {/* Desktop menu */}
+      <ul className="hidden md:flex">
         <li>Home</li>
         <li>About</li>
         <li>Skills</li>
@@ -22,20 +28,24 @@ const NavBar = () => {
         <li>Contact Us</li>
       </ul>
 
-      {/* dropdown Menu */}
+      {/* Mobile menu */}
       <div onClick={handleClick} className="md:hidden z-10">
         <FaBars />
       </div>
-      {/* Mobile menu */}
-      <ul className={nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
-        <li className="py6 text-4xl"> Home</li>
-        <li className="py6 text-4xl">About</li>
-        <li className="py6 text-4xl">Skills</li>
-        <li className="py6 text-4xl">Projects</li>
-        <li className="py6 text-4xl">Contact Us</li>
-      </ul>
+
+      {/* Mobile dropdown menu */}
+      {nav && (
+        <ul className="md:hidden absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center">
+          <li className="py-6 text-4xl">Home</li>
+          <li className="py-6 text-4xl">About</li>
+          <li className="py-6 text-4xl">Skills</li>
+          <li className="py-6 text-4xl">Projects</li>
+          <li className="py-6 text-4xl">Contact Us</li>
+        </ul>
+      )}
     </div>
   );
 };
 
 export default NavBar;
+
