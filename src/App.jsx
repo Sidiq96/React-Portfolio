b88062a73a0847b8b2440a33549c20f7adda521f
Sidiq Hersi <hersisidiq@gmail.com>
2024-01-08 16:37:14 +0000

    adds code for the navbar and adds mobile version too

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
+import React from "react";
+import NavBar from "../components/NavBar";
 
 function App() {
-
-
   return (
-    <>
-      <h1 className="text-3xl font-bold">Hello</h1>
-    </>
-  )
+    <div>
+      <NavBar />
+    </div>
+  );
 }
 
-export default App
+export default App;
