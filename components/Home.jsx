e4dc2d456ffe27d4e58c0a8194894a4c9be1b41a
Sidiq Hersi <hersisidiq@gmail.com>
2024-01-09 00:08:04 +0000

    adds line code for hover effects on the menu bar and formats the code

--- components/Home.jsx
+++ components/Home.jsx
@@ -7,14 +7,14 @@ const Home = () => {
       {/* Container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
         {/* Your content goes here */}
-        <p className="text-pink-500">Hi, My Name is</p>
+        <p className="text-red-500">Hi, My Name is</p>
         <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
           Sidiq Hersi
         </h1>
         <h2 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
           I'm a Front-end Developer
         </h2>
-        <p className="text-[#8892b0] py-4 max-w-[700px]">
+        <p className="text-[#e7ebf6] py-4 max-w-[700px]">
           Passionate front-end web developer creating engaging and responsive
           user interfaces to bring digital experiences to life.{" "}
         </p>
--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -13,6 +13,7 @@ const NavBar = () => {
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
         <img
+          className="cursor-pointer"
           src={Logo}
           alt="Logo Image"
           style={{ width: "110px", height: "110px" }}
@@ -21,26 +22,26 @@ const NavBar = () => {
 
       {/* Desktop menu */}
       <ul className="hidden md:flex">
-        <li>Home</li>
-        <li>About</li>
-        <li>Skills</li>
-        <li>Projects</li>
-        <li>Contact Us</li>
+        <li className="hover:text-red-500">Home</li>
+        <li className="hover:text-red-500">About</li>
+        <li className="hover:text-red-500">Skills</li>
+        <li className="hover:text-red-500">Projects</li>
+        <li className="hover:text-red-500">Contact Us</li>
       </ul>
 
       {/* Mobile menu */}
-      <div onClick={handleClick} className="md:hidden z-10">
+      <div onClick={handleClick} className="md:hidden z-10 hover:text-red-500">
         <FaBars />
       </div>
 
       {/* Mobile dropdown menu */}
       {nav && (
         <ul className="md:hidden absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center">
-          <li className="py-6 text-4xl">Home</li>
-          <li className="py-6 text-4xl">About</li>
-          <li className="py-6 text-4xl">Skills</li>
-          <li className="py-6 text-4xl">Projects</li>
-          <li className="py-6 text-4xl">Contact Us</li>
+          <li className="py-6 text-4xl hover:text-red-500">Home</li>
+          <li className="py-6 text-4xl  hover:text-red-500">About</li>
+          <li className="py-6 text-4xl  hover:text-red-500">Skills</li>
+          <li className="py-6 text-4xl  hover:text-red-500">Projects</li>
+          <li className="py-6 text-4xl  hover:text-red-500">Contact Us</li>
         </ul>
       )}
     </div>
@@ -48,4 +49,3 @@ const NavBar = () => {
 };
 
 export default NavBar;
-
