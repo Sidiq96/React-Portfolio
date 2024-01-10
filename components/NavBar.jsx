62508f5d3d5734e5b1e16b9ec2d42c8adbcbe525
Sidiq Hersi <hersisidiq@gmail.com>
2024-01-10 14:17:26 +0000

    adds code when the logo is clicked, you are taken back to the homepage

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -8,6 +8,9 @@ const NavBar = () => {
   const handleClick = () => {
     setNav(!nav);
   };
+  const handleClickLink = () => {
+    window.location.href = "/";
+  }
 
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300 ">
@@ -17,6 +20,7 @@ const NavBar = () => {
           src={Logo}
           alt="Logo Image"
           style={{ width: "110px", height: "110px" }}
+          onClick={handleClickLink}
         />
       </div>
 
